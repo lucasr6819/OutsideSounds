edbaaa5e8a07fb18ec73dc9dd32b5c0312b455b2
lucasr6819 <144037375+lucasr6819@users.noreply.github.com>
2025-06-07 16:11:44 -0400

    Add files via upload

---
+++ EPSCOR WEBSITE/map.js
@@ -0,0 +1,9 @@
+let audioContext;
+let currentVisualizer;
+
+// All the functionality code goes here - including:
+// - Audio visualization
+// - Map initialization
+// - Marker creation
+// - Event handlers
+// - Data processing
